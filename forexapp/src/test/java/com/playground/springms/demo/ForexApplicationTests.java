1f5bbf8e396efbabb21af80348934b72a979256f
Junaid Khan <m.junaiid.khan@gmail.com>
2018-10-15 12:00:00 +0400

    initial commit

---
+++ forexapp/src/test/java/com/playground/springms/demo/ForexApplicationTests.java
@@ -0,0 +1,16 @@
+package com.playground.springms.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForexApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
