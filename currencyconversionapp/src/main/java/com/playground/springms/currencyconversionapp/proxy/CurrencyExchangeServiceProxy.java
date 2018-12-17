1266c10e4dfcbf54fb45ce18b177382d463499f6
Junaid Khan <m.junaiid.khan@gmail.com>
2018-12-17 11:16:58 +0400

    eureka

--- currencyconversionapp/src/main/java/com/playground/springms/currencyconversionapp/proxy/CurrencyExchangeServiceProxy.java
+++ currencyconversionapp/src/main/java/com/playground/springms/currencyconversionapp/proxy/CurrencyExchangeServiceProxy.java
@@ -1,13 +1,15 @@
 package com.playground.springms.currencyconversionapp.proxy;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.playground.springms.currencyconversionapp.beans.CurrencyConversionBean;
 
-@FeignClient(name = "forex-service", url = "localhost:8000/forexapp")
+//@FeignClient(name = "forex-service", url = "localhost:8000/forexapp")
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
